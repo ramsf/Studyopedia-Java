d1c4f2334e353b7056079ff8815413c49bf712d3
Luciano Cleistenes <123988661+Cleistenes@users.noreply.github.com>
2023-01-31 21:17:20 +0000

    Add files via upload

---
+++ comandosdeimpressaoevariaveis.java
@@ -0,0 +1,12 @@
+public class comandosdeimpressaoevariaveis {
+    public static void main(String[] args){
+
+        int n1=10;
+        int n2=20;
+        int n3=30;
+        int res=(n1+n2+n3)/3;
+        String nome="Luciano";
+
+        System.out.printf("Valor da variavel res e: %d%nExercicio de: %s",res, nome);
+    }
+}
