4bb44eb24dc5154655fd9e19f697ad4f17a52cb0
Luciano Cleistenes <123988661+Cleistenes@users.noreply.github.com>
2023-02-02 22:38:23 +0000

    Add files via upload

---
+++ comandosdedecisao.java
@@ -0,0 +1,21 @@
+public class comandosdedecisao {
+    public static void main(String[] args){
+        int prova1 = 55;
+        int prova2 = 60;
+        int prova3 = 90;
+        int media = (prova1+prova2+prova3) / 3;
+        int resultadofinal = media;
+        int faltas = 15;
+        int maxFaltas = 12;
+        System.out.println("Resultado final: " + resultadofinal + " quantidade de faltas: " + faltas);
+        if(resultadofinal >= 65){
+            System.out.println("Aprovado!");
+        }else if(resultadofinal >=40){
+            System.out.println("Recuperacao");
+        }
+        else{
+            System.out.println("Reprovado.");
+        }
+        System.out.println("Fim do programa");
+    }
+}
